57ac4a7e5dc1dccc1082bad8d43dc693533ce8c1
shivas1432 <shivas1432@users.noreply.github.com>
2024-01-03 14:58:12 +0000

    Implement pagination

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -1,3 +1,4 @@
+// Enhanced: Implement pagination - Jan 2024
 'use client';
 
 import * as React from 'react';
@@ -153,3 +154,4 @@ export {
   CommandShortcut,
   CommandSeparator,
 };
+
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+// Enhanced: Implement pagination - Jan 2024
 // Enhanced: Implement search functionality - Jan 2024
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
@@ -5,4 +6,5 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
 
