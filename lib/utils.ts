65172632c391ae3c311e5e0ea4e2561da99727b7
shivas1432 <shivas1432@users.noreply.github.com>
2024-01-06 09:37:53 +0000

    Fix API response formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+// Enhanced: Fix API response formatting - Jan 2024
 // Enhanced: Implement pagination - Jan 2024
 // Enhanced: Implement search functionality - Jan 2024
 import { clsx, type ClassValue } from 'clsx';
@@ -6,5 +7,6 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
 
 
